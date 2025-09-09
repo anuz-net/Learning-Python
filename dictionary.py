62bf108912466ea0d079c880be66b08c689f8083
Anuz <95576641+anuz-net@users.noreply.github.com>
2025-09-09 19:21:27 +0545

    main

--- dictionary.py
+++ dictionary.py
@@ -1,6 +1,15 @@
 # Structure
 
 d = {
-    "Faculty" : "BCA"
+    "Faculty" : "BCA",
+    "Name" : "Anuj Dhakal",
+    "RollNO" : 2
     
 }
+print(d)
+print(d["Faculty"])
+print(len(d))
+x  = d.get("Name") 
+y = d.key()
+print(x)
+print(y)
